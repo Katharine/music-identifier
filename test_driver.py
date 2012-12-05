dc589d5387028efc8e75daf87d4b01e8b03eb6e4
Katharine Berry <katharine@kathar.in>
2012-12-05 13:35:36 -0500

    Driver no longer prompts for folder, controls iTunes.

--- test_driver.py
+++ test_driver.py
@@ -3,17 +3,28 @@
 import storage
 import guess
 import os
+import audiotools
+import appscript
 
 def test_directory(d):
-    song_list = [d + '/' + x for x in os.listdir(d)]
-
-    for filename in song_list:
-        storage.store_file(filename)
-        print "Stored %s (%d unique samples)" % (filename, len(storage.HashTable))
+    store = storage.HashStore()
+    for root, bar, files in os.walk(d):
+        for filename in files:
+            filename = root + '/' + filename
+            try:
+                store.store_file(filename)
+                print "Stored %s" % filename
+            except audiotools.UnsupportedFile:
+                print "Skipping unsupported file %s" % filename
+            except Exception, e:
+                print e
 
 def main():
-    audio_dir = raw_input("Enter audio directory: ")
-    test_directory(audio_dir)
+    #audio_dir = raw_input("Enter audio directory: ")
+    #test_directory(audio_dir)
+    itunes = appscript.app('iTunes')
+    library = itunes.playlists['Library']
+
 
     while True:
         inp = raw_input("Press return to start listening. Type 'quit' when you're bored.")
@@ -33,6 +44,11 @@ def main():
             seconds = time % 60
             print "%d:%d into song" % (minutes, seconds)
             print ""
+            itunes_track = library.search(for_=song.track_name + ' ' + song.artist)
+            if len(itunes_track) > 0:
+                itunes.play(itunes_track[0], once=True)
+                itunes.player_position.set(time + 0.05)
+
 
 if __name__ == '__main__':
     main()
